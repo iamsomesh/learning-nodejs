12641e8f60dcf64de729099d0a82122e481029a6
Somesh Yadav <syb777yadav@gmail.com>
2023-03-01 01:45:36 +0530

    Routing requests

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-//creating node server
+const http = require("http");
 
-const http = require("http"); //http is global module which I import using require keyword
-
-//createServer is callback/function which takes also a anonymous function for taking a request and send response
 const server = http.createServer((req, res) => {
-  // console.log(req);
-  
-  console.log(req.url, req.method, req.headers)//let's o/p request url, method and header
-
-  //send response to server
-  res.setHeader('Content-Type', 'text/html'); //setHeader method send response into header
-  res.write('<html>'); //write some data to response
+  if (req.url === '/'){
+    res.write('<html>');
+    res.write('<head><title>input form</title></head>');
+    res.write('<body><form action = "/message" method = "POST"><input type = "text" name = "name"><button type = "submit">send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
   res.write('<head><title>my first page</title></head>');
   res.write('<body><h1>This is my first node js application response</h1></body>');
   res.write('</html>');
-
+  res.end();
 });
 
-server.listen(3000) //listen is another function which listen this function also it takes argument as port number where our server is listen and it's optional
-
-//to run this server it - node app.js and hit localhost:3000 in browser to takes incoming request from browser
+server.listen(3000)
