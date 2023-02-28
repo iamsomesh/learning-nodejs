bf9c9d44d3edfff7066f297da9593d06a87fba5c
Somesh Yadav <syb777yadav@gmail.com>
2023-03-01 01:04:39 +0530

    console new reuest o/p url, method and header

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const http = require("http"); //http is global module which I import using requi
 
 //createServer is callback/function which takes also a anonymous function for taking a request and send response
 const server = http.createServer((req, res) => {
-  console.log(req);
+  // console.log(req);
+  
+  console.log(req.url, req.method, req.headers)//let's o/p request url, method and header
+
 });
 
 server.listen(3000) //listen is another function which listen this function also it takes argument as port number where our server is listen and it's optional
